5ed8551cb8d3806c31d157cba68554cea5317fd5
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-01-29 11:24:07 -0600

    Added missing files from last commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = "uri",
+    version = "1.0",
+    description = "A library for URI handling featuring an implementation of URI-Templates",
+    author = 'Jacob Kaplan-Moss',
+    author_email = 'jacob@jaobian.org',
+    py_modules = ['uri'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+    ]
+)
